e5ca89dbb14ccfcb3c24e41b3bce1647f70a788f
Forrest Corcoran <fpcorcoran17@gmail.com>
2020-07-28 08:45:14 -0400

    to_egm() takes user input sampling size

--- PyCESat2/Beam.py
+++ PyCESat2/Beam.py
@@ -14,9 +14,36 @@ class beamObject:
 		self.lat = lat
 		self.lon = lon
 
-	def filter(self, win_x=3.0, win_h=3.0, threshold=15):
+	def filter(self, win_x=3.0, win_y=3.0, threshold=15):
 		"""
+		Remove low density photon regions.
 
+		Description:
+		------------
+    	Applies a moving window function of dimension (win_x,win_y) to the beam
+		photons. Windows containing less than the threshold number of photons
+		are removed. The window starts from the top left track, moving down. On
+		reaching the bottom, the window is moved to the right by win_x and
+		repositioned at the top.
+
+		NOTE: Edge effects are present on the right side of the track for
+			  situations where track_width % win_x != 0. Suggestions for
+			  minimizing edge effects are welcome. Please raise as issue before
+			  submitting pull request.
+
+    	Parameters:
+		-----------
+    		win_x (float) 	- X dimension of the moving window filter.
+
+			win_y (float) 	- Y dimension of the moving window filter.
+
+			threshold (int) - Minimum number of photons needed within filter to
+			 				  avoid filtering
+
+
+    	Returns:
+		--------
+    		beamObject
 		"""
 
 		#get along track range
@@ -52,7 +79,7 @@ class beamObject:
 			#proceed through depth for each X window
 			while y_start >= h_min:
 				#define bottom of depth window
-				y_stop = y_start - win_h
+				y_stop = y_start - win_y
 
 				#get the photons inside the window
 				photons = xy.loc[(xy["height"]<=y_start) \
@@ -91,7 +118,38 @@ class beamObject:
 		return beamObject(filteredPhotons[:,0],filteredPhotons[:,1],
 						  filteredPhotons[:,2],filteredPhotons[:,3])
 
-	def to_egm08(self, interpolate_geoid=True):
+	def to_egm08(self, interpolate_geoid=True, sample_n=10):
+		"""
+		Convert ellipsoid referenced elevations to geoid referenced elevations.
+
+		Description:
+		------------
+    	ATLAS measures photon elevations referenced from the ellipsoid, however
+		it is often more convenient to analyze elevations referenced from the
+		geoid.
+
+		This function currently supports the EGM08 geoid model. EGM20 will be
+		added as an update upon its public release.
+
+
+    	Parameters:
+		-----------
+    		interpolate_geoid (bool) - To improve processing speed, the geoid
+									   can be interpolated by sampling lat/lon
+									   along the ground track. If TRUE, a cubic
+									   spline is fit to the sample lat/lons and
+									   photon heights are calculated from the s
+									   pline. Default=TRUE.
+
+			sample_n (int) 			- The number of sample lat/lons to use for the
+							  		  interpolated geoid spline. Default=10.
+
+
+    	Returns:
+		--------
+    		beamObject
+		"""
+
 		#path to geoid model file
 		geoid = os.path.join(os.path.dirname(__file__),"data","EGM08","EGM2008_mosaic.tif")
 
@@ -103,8 +161,8 @@ class beamObject:
 
 
 			latlon_samples = list(zip(
-									np.linspace(lat_min, lat_max, 500),
-									np.linspace(lon_min, lon_max, 500)
+									np.linspace(lat_min, lat_max, sample_n),
+									np.linspace(lon_min, lon_max, sample_n)
 									))
 
 			undulations=[]
@@ -140,7 +198,7 @@ class beamObject:
 			return beamObject(self.height + np.asarray(point_query(pts,geoid)),
 							  self.distance, self.lat, self.lon)
 
-	def bin(self, win_x=3.0, win_h=3.0):
+	def bin(self, win_x=3.0, win_y=3.0):
 		#get along track range
 		track_min = np.min(self.distance)
 		track_max = np.max(self.distance)
@@ -172,7 +230,7 @@ class beamObject:
 			#proceed through depth for each X window
 			while y_start >= h_min:
 				#define bottom of depth window
-				y_stop = y_start - win_h
+				y_stop = y_start - win_y
 
 				#get number of photons inside current window
 				bin_n = len(xy.loc[
