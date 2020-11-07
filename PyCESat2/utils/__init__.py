f86cc9e2825990e6cfb0335afead23db3f8f488d
Forrest Corcoran <fpcorcoran17@gmail.com>
2020-11-07 09:28:19 -0800

    Added support for scipy distributions.

--- PyCESat2/utils/__init__.py
+++ PyCESat2/utils/__init__.py
@@ -1,3 +1,4 @@
 from .curve_utils import *
 from .surface_utils import *
 from .statistics import mean, mode, median, stdev, skewness, kurtosis, variance, pearson_skewness, AUC
+from .distribution import distributions
